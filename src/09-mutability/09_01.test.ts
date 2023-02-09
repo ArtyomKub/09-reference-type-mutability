aaa303eca6bd8b1a0ee0b618aabd8924b77ffc20
ArtyomKub <kub.killjoy@gmail.com>
2023-02-09 11:43:26 +0300

    Start learn mutability

---
+++ src/09-mutability/09_01.test.ts
@@ -0,0 +1,18 @@
+function icreaseAge(user) {
+    user.age++;
+}
+
+type UserType = {
+    name: string
+    age: number
+}
+
+test('big test', ()=>{
+    var user = {
+        name: 'Artyom',
+        age: 32
+    }
+
+
+
+})
---
+++ src/09-mutability/09_01.ts
@@ -0,0 +1,4 @@
+var user = {
+    name: 'Artyom',
+    age: 32
+}
